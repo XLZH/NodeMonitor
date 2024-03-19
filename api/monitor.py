0c028d5c9e3659e5c8f98296631a792eeb06999e
xiaolong zhang <xiaolongzhang2015@163.com>
2024-03-19 12:38:58 +0800

    feat: add necessary API for monitor

--- api/monitor.py
+++ api/monitor.py
@@ -19,11 +19,82 @@ __all__ = ['monitor']
 monitor = APIRouter()
 
 
-@monitor.get("/nodes", summary="get the server information for all the nodes")
-async def get_nodes_info():
+@monitor.get("/nodes/info60",
+             summary="get the server information within 60 seconds for all nodes")
+async def get_nodes_all60():
     """
     get the server information (cpu, mem, disk, et.) for all the nodes
     """
-    node_list = await NodeModel.all()
+    info_list = await NodeModel.all().values(
+        'host', 'cpu_60', 'mem_60', 'net_rx_60', 'net_tx_60'
+    )
 
-    return {'count': len(node_list), 'node_list': node_list}
+    return {'count': len(info_list), 'info_list': info_list}
+
+
+@monitor.get("/node/info5",
+             summary="get the server information within 5 seconds for given node")
+async def get_node_info5(host: str):
+    """
+    get the server information within 5 seconds for given hostname
+    """
+    node_info = await NodeModel.filter(host=host).values(
+        'host', 'cpu_5', 'mem_5', 'net_rx_5', 'net_tx_5'
+    )
+
+    if len(node_info) == 0:
+        raise HTTPException(
+            status_code=410,
+            detail=f"unknown hostname ({host}) is detected!"
+        )
+
+    return {'count': 1, 'info5': node_info}
+
+
+@monitor.get("/node/info60",
+             summary="get the server information within 60 seconds for given node")
+async def get_node_info60(host: str):
+    """
+    get the server information within 60 seconds for given hostname
+    """
+    node_info = await NodeModel.filter(host=host).values(
+        'host', 'cpu_60', 'mem_60', 'net_rx_60', 'net_tx_60'
+    )
+
+    if len(node_info) == 0:
+        raise HTTPException(
+            status_code=410,
+            detail=f"unknown hostname ({host}) is detected!"
+        )
+
+    return {'count': 1, 'info60': node_info}
+
+
+@monitor.get("/nodes/disk", summary="get the disk status for all nodes")
+async def get_nodes_disk():
+    """
+    get the disk status for all nodes
+    """
+    disk_list = await NodeModel.all().values(
+        'host', 'disk_status', 'disk_failed'
+    )
+
+    return {'count': len(disk_list), 'node_disk_list': disk_list}
+
+
+@monitor.get("/node/disk", summary="get the disk status for given node")
+async def get_node_disk(host: str):
+    """
+    get the disk status for given hostname
+    """
+    disk_info = await NodeModel.filter(host=host).values(
+        'host', 'disk_status', 'disk_failed'
+    )
+
+    if len(disk_info) == 0:
+        raise HTTPException(
+            status_code=410,
+            detail=f"unknown hostname ({host}) is detected!"
+        )
+
+    return {'count': 1, 'disk': disk_info}
